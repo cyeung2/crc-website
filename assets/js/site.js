d73138c3b5f3b0bbd00ba4674f3e9ec1c6ecf267
WANMIN ZHANG <wzhang90@hawk.iit.edu>
2020-05-03 22:56:19 -0500

    Correct homepage and issues page

--- assets/js/site.js
+++
@@ -1,5 +0,0 @@
-let mainNav = document.getElementById('navigation');
-let navToggle = document.getElementById('js-toggle');
-navTogle.addEventListener('click', function () {
-  mainNav.classList.toggle('active');
-});
